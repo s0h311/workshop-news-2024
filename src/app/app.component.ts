bd2ada321bdc61f0e7f0b53ddd8f3dc74a5797c6
Soheil Nazari <kontakt@soheilnazari.de>
2024-10-18 20:25:28 +0200

    add dialog, use pnpm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { of } from 'rxjs';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DialogComponent, AsyncPipe, DatePipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'news-10-24';
+  public userData = of({
+    id: crypto.randomUUID(),
+    name: 'James',
+    lastName: 'Bond',
+    birthDate: new Date(1999, 1, 1),
+  });
 }
